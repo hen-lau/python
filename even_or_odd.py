3e2f611bd5bed9e6218d074c65351dd72e7ea263
Troy McFritte <github@lausnet.de>
2025-11-11 21:36:33 +0100

    First Commit

---
+++ even_or_odd.py
@@ -0,0 +1,9 @@
+print("Gib eine Zahl ein")
+eingegebeneZahl = int(input())
+print(f"Deine eingegebene Zahl ist {eingegebeneZahl}")
+
+eingegebeneZahlDiv = (eingegebeneZahl/2)
+
+if(eingegebeneZahl){
+    
+}
---
+++ greeting.py
@@ -0,0 +1,5 @@
+# Das Programm soll den User nach seinem oder ihrem Namen fragen und dann persönlich begrüßen.
+
+print("Hallo! Wie ist dein Name?")
+name = input()
+print(f"Hallo {name}!")
